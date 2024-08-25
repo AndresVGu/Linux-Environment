c98aee07ba0caaaced265525ed71763f0183fc61
Andres Arturo Villarreal Gutierrez <avillarrealguti1@ncstudents.niagaracollege.ca>
2024-08-24 20:40:46 -0400

    First Installation

--- setup.sh
+++ setup.sh
@@ -13,7 +13,9 @@ turquoiseColor="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
 #Global Variables:
-
+dir=$(pwd)
+fdir="$HOME/.local/share/fonts"
+user=$(whoami)
 
 #Exit Process:
 trap ctrl_c INT
@@ -22,3 +24,26 @@ function ctrl_c(){
     echo -e "\n\n${redColor}[!] Ending Process...\n${endColor}"
     exit 1
 }
+
+function banner(){
+
+}
+
+if [ "$user" == "root" ]; then
+    banner
+    echo -e "\n\n${redColor}[!] You should not run this script as root...${endColor}"
+    exit 1
+else
+    banner
+    sleep 1
+    echo -e "\n\n${greenColor}[+] Installing necessary packages...\n${endColor}"
+    sleep 2
+    #First Installation 
+    sudo apt install -y kitty rofi feh xclip ranger i3lock-fancy scrot scrub wmname imagemagick cmatrix htop neofetch python3-pip procps tty-clock fzf lsd bat pamixer flameshot
+    if [ $? != 0 ] && [ $? != 130 ]; then
+        echo -e "\n${redColor}[!] Failed Installing Packages!\n${endColor}"
+        exit1
+    else    
+        echo -e "\n${greenColor}[+] Installation Successfull...\n${endColor}"
+        sleep 1.5
+    fi
